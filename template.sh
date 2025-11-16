e438897a56d455d5d5ebe3d8a88f07e3b6c47749
Boubacar Diallo <boubaa.bd@gmail.com>
2025-11-16 21:20:53 +0000

    folder structure added

---
+++ template.sh
@@ -0,0 +1,19 @@
+!/bin/bash
+
+# création des répertoires
+mkdir -p src
+mkdir -p research
+
+# création des fichiers
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Répertoires et fichiers créés avec succès!"
+
+
